4523f95ea89b4ee7ce77fe23910dfabf06fe9eea
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-20 19:07:13 +0100

    clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen w-screen">
+	<div class="grid h-full grid-cols-2">
+		<div id="bg-img"></div>
+		<div class="bg-black">hi</div>
+	</div>
+</div>
+
+<style>
+	#bg-img {
+		background-image: url('/tree.webp');
+		box-shadow:
+			50px 50px 113px #000 inset,
+			-50px -50px 110px #000 inset;
+	}
+</style>
