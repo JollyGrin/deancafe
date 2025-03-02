9fbf77acc075801cb21d53a50cd0d787192b995d
JollyGrin <lol@dean.lol>
2025-03-02 04:07:57 +0100

    add shaders

---
+++ src/lib/shader/ShaderCanvas.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import ShaderScene from './ShaderScene.svelte';
+	import { shaderConfigWarpGrid } from './shaders/shader-warpgrid';
+
+	let { shader }: { shader: typeof shaderConfigWarpGrid } = $props();
+</script>
+
+<div class="h-full w-full">
+	<Canvas>
+		<ShaderScene {shader} />
+	</Canvas>
+</div>
---
+++ src/lib/shader/shaders/index.ts
@@ -0,0 +1,4 @@
+export * from './shader-mdr';
+export * from './shader-warpgrid';
+export * from './shader-rainbow';
+export * from './shader-watergreen';
